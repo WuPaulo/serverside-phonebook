632b7646eefdcfe5f88b7c6609f01c21857bf01c
WuPaulo <paulowbusiness@gmail.com>
2021-05-29 16:55:23 -0400

    heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 var morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 let persons = [
   {
