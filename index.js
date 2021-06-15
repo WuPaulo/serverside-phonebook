d9a59930c445420ddfeab395a122cfb8624a127a
WuPaulo <paulowbusiness@gmail.com>
2021-06-14 22:05:10 -0400

    heroku

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ let info = [
 ];
 
 app.get("/", (request, response) => {
-  response.send("<h1>Phonebook</h1>");
+  response.send("<h1>Phonebooks</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
@@ -111,7 +111,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
